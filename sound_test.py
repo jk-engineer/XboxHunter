fee7ac6d73049d2e2ca45a19165c84cdd9e8b7bf
jk-engineer <jk-218@yandex.ru>
2021-08-10 20:49:43 +0300

    Добавлен тест звукового оповещения

---
+++ sound_test.py
@@ -0,0 +1,22 @@
+# Программа для проверки звукового оповещения
+# Copyright (C) 2021 Evgeniy Ipatov
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+import winsound
+
+
+for index in range(0, 10):
+    winsound.Beep(800, 1000)
