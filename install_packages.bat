c07e7aadb4e78b08c44cdfb9a08a6e7651cbcc49
jk-engineer <jk-218@yandex.ru>
2021-08-17 20:26:59 +0300

    Добавлены файлы для запуска установки пакетов и основной программы

---
+++ install_packages.bat
@@ -0,0 +1,10 @@
+::Скопируйте файл install_packages.bat в директорию с файлом pip.exe
+::Например:
+::C:\Program Files (x86)\Python3\Scripts\pip.exe
+::Запустите файл install_packages.bat для установки перечисленных ниже пакетов
+
+pip.exe install beautifulsoup4
+pip.exe install requests
+pip.exe install requests-html
+pip.exe install certifi
+pause
---
+++ xbox_hunter.bat
@@ -0,0 +1,4 @@
+::Отредактируйте в строке ниже путь к файлу python.exe
+
+"C:\Program Files (x86)\Python3\python.exe" "xbox_hunter.py"
+pause
